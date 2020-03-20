43280e7c24d84d07b8f1a05606e883dae0fa3a0c
@ali25rohmansyah <ali25rohmansyah@gmail.com>
2020-03-20 09:58:29 +0700

    add camera to take a picture and predict the image

--- app/src/main/java/com/example/ra_recipes/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/ra_recipes/ui/search/SearchFragment.kt
@@ -1,111 +1,127 @@
 package com.example.ra_recipes.ui.search
 
-import android.Manifest
-import android.app.Activity.RESULT_OK
+import android.app.AlertDialog
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.os.Build
+import android.os.AsyncTask
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
+import androidx.annotation.UiThread
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import clarifai2.api.ClarifaiBuilder
+import clarifai2.dto.input.ClarifaiInput
+import clarifai2.dto.model.output.ClarifaiOutput
+import clarifai2.dto.prediction.Concept
 import com.example.ra_recipes.R
-import com.example.ra_recipes.adapter.SelectedImageAdapter
 import com.example.ra_recipes.databinding.FragmentSearchBinding
+import dmax.dialog.SpotsDialog
 import kotlinx.android.synthetic.main.fragment_search.*
+import java.io.File
+import java.io.IOException
 
 class SearchFragment : Fragment() {
-
-    private val PERMISSION_CODE: Int = 1000
-    private val IMAGE_CAPTURE_CODE = 1001
     private lateinit var searchViewModel: SearchViewModel
+    private lateinit var status: String
+    private var photoPath: String? = null
+    private lateinit var dialog:AlertDialog
 
-    private lateinit var viewAdapter: SelectedImageAdapter
-    private lateinit var viewManager: RecyclerView.LayoutManager
-    private lateinit var viewModel: SearchViewModel
-//    private val image = mutableListOf<Bitmap>()
+
+    private inner class ClarifaiTask : AsyncTask<File?, Int?, Boolean>() {
+
+        override fun doInBackground(vararg images: File?): Boolean? {
+            val client = ClarifaiBuilder("ce702309aab54ca8a279c2693210701c").buildSync()
+            var predictionResults: List<ClarifaiOutput<Concept>>
+            for (image in images) {
+                predictionResults = client.defaultModels.generalModel().predict()
+                    .withInputs(ClarifaiInput.forImage(image!!))
+                    .executeSync()
+                    .get()
+                for (result in predictionResults) {
+                    for (datum in result.data()) {
+
+                        activity!!.runOnUiThread {
+                            textView2.append(datum.name()+"\n")
+                        }
+//                        if (datum.name()!!.contains("phone")) return true
+                    }
+                }
+            }
+            return false
+        }
+        override fun onPostExecute(result: Boolean) {
+            // Delete photo
+            File(photoPath!!).delete()
+            photoPath = null
+
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        searchViewModel =
-            ViewModelProviders.of(this).get(SearchViewModel::class.java)
+
         val binding: FragmentSearchBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_search, container, false
         )
-        viewModel = ViewModelProviders.of(this).get(searchViewModel::class.java)
-        viewManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        viewAdapter = SelectedImageAdapter(viewModel)
 
-        binding.selectedRecyclerView.apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
+        //ViewModel
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
+        binding.searchViewModel = searchViewModel
 
-        }
-        binding.btnImg.setOnClickListener {
-            //check permission
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if(ActivityCompat.checkSelfPermission(context!!, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
-                    ActivityCompat.checkSelfPermission(context!!, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ){
+        searchViewModel.status.observe(this, Observer { statusMsg ->
+            status = statusMsg
+            binding.textView2.text = statusMsg
+            if (status != "processing an image") dialog.dismiss()
+        })
 
-                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    requestPermissions(permission, PERMISSION_CODE)
-                }else{
-                    //Open Camera
-                    openCamera()
+        binding.btnImg.setOnClickListener {val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (takePictureIntent.resolveActivity(activity!!.packageManager) != null) {
+                val photoFile: File
+                try {
+                    val storageDir = activity!!.filesDir
+                    photoFile = File.createTempFile(
+                        "SNAPSHOT",
+                        ".jpg",
+                        storageDir
+                    )
+                    photoPath = photoFile.absolutePath
+                }catch (ex: IOException){
+                    return@setOnClickListener
                 }
+                val photoURI = FileProvider.getUriForFile(requireContext(),
+                    "com.example.ra_recipes.fileprovider",
+                    photoFile)
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
             }
+
         }
-        binding.btnDone.setOnClickListener {
-            if (viewModel.image.value!!.size <= 0) {
-                Toast.makeText(activity, "Select at least one image", Toast.LENGTH_SHORT).show()
-            } else{
-                findNavController().navigate(R.id.action_navigation_home_to_recipesFilterFragment)
-            }
-        }
+
         return binding.root
     }
 
-    private fun openCamera() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    override fun onResume() {
+        super.onResume()
+        if (photoPath != null){
+            dialog = SpotsDialog.Builder()
+                .setContext(requireContext())
+                .setMessage(status)
+                .setCancelable(false)
+                .build()
+            dialog.show()
+            ClarifaiTask().execute(File(photoPath!!))
+        }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when(requestCode){
-            PERMISSION_CODE -> {
-                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_DENIED){
-                    openCamera()
-                }else{
-                    Toast.makeText(activity, "permission denied", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-        if (resultCode == RESULT_OK) {
-            imageView.setImageURI(data?.data)
-            val imageBitmap = data.extras?.get("data") as Bitmap
-            viewModel.image.value!!.add(imageBitmap)
-        }
+    companion object {
+        const val REQUEST_TAKE_PHOTO = 1
     }
 }
